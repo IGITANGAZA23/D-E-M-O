eaf5bf828484bbad9aea2bc766e6bdac9cdc4ec8
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-11 09:21:52 -0800

    fix: resolve issue with DB port

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { BorrowRecord } from './borrow-records/entities/borrow-record.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 5432,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_DATABASE || 'library_management',
