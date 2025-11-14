597a3002eb2612b22743729630cb172ecc7a66e2
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-14 13:30:16 -0800

    feat: add swagger decorators to dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,14 @@
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'strongpassword123' })
   @IsString()
   @IsNotEmpty()
   password: string;
 }
-
