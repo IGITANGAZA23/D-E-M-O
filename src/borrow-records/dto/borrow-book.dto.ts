2846b6e234fb7cfc7eb0c370ae22806716954fea
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-14 21:29:50 -0800

    feat: add apiproprty to the borrow-book.to.ts

--- src/borrow-records/dto/borrow-book.dto.ts
+++ src/borrow-records/dto/borrow-book.dto.ts
@@ -1,8 +1,8 @@
 import { IsInt, IsNotEmpty } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class BorrowBookDto {
+  @ApiProperty({ example: 1 })
   @IsInt()
   @IsNotEmpty()
   bookId: number;
 }
-
