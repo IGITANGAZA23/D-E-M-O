d679208027f03bd5e97a59362ef9872fba5049be
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-14 21:34:06 -0800

    feat: add apiproperty to create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,19 @@
 import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+  @ApiProperty({ example: 'email@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'securePassword' })
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({ example: 'John Doe' })
   @IsString()
   @IsNotEmpty()
   name: string;
 }
-
