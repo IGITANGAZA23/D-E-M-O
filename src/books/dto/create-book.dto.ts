7873eeebc5169966f20f1c11161d085704de40dc
IGITANGAZA23 <igitangazanobleprince@gmail.com>
2025-11-14 21:27:39 -0800

    feat: add ApiProperty to the create-book.dto.ts

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,19 +1,23 @@
 import { IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
 import { BookStatus } from '../entities/book.entity';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateBookDto {
+  @ApiProperty({ example: 'The Great Gatsby' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ example: 'F. Scott Fitzgerald' })
   @IsString()
   @IsNotEmpty()
   author: string;
 
+  @ApiProperty({ example: '789439848939' })
   @IsString()
   @IsNotEmpty()
   isbn: string;
 
+  @ApiProperty({ example: 'available', enum: BookStatus, required: false })
   @IsOptional()
   @IsEnum(BookStatus)
   status?: BookStatus;
